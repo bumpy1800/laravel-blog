ffd29396431fe7fb42f96098f5b2ad83503b5026
bumpy1800 <bumpy1800@gmail.com>
2021-12-27 15:57:45 +0900

    UPDATE : main_page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,47 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors')
 
 module.exports = {
-    mode: 'jit',
+    mode : 'jit',
     purge: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
     ],
-
+    darkMode: false, // or 'media' or 'class'
     theme: {
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            black: colors.black,
+            white: colors.white,
+            gray: colors.trueGray,
+            indigo: colors.indigo,
+            red: colors.rose,
+            yellow: colors.amber,
+            emerald: colors.emerald,
+          },
         extend: {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
+            width: {
+                '1/7': '14.2857143%',
+                '2/7': '28.5714286%',
+                '3/7': '42.8571429%',
+                '4/7': '57.1428571%',
+                '5/7': '71.4285714%',
+                '6/7': '85.7142857%',
+              }
         },
     },
-
+    variants: {
+        extend: {
+            fill: ['hover', 'focus'],
+          },
+      },
     plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 };
